20f9d6fa1765a3c177662a86aea03442b506de16
Salma490 <asrsalma5732590@gmail.com>
2025-06-10 12:59:27 +0530

    May 28th Wednesday Practicals

---
+++ Functions/addNumbers.m
@@ -0,0 +1,6 @@
+function sum = addNumbers(x, y)
+    sum = x + y;
+end
+
+%when you first save the file, then type you can put whatever name for file.
+%but after typing you are saving you have to save with function name.
