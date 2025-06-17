b52d281047b1194d2f4b1df4835009ab630f03d6
Salma <asrsalma5732590@gmail.com>
2025-06-17 14:49:17 +0530

    Graph Practicals

--- Graphs/graph1.m
+++ Graphs/graph1.m
@@ -11,10 +11,9 @@ title('Undirected Graph');
 
 arr = [1, 2, 5, 8, 7];
 
-
 s=[1 2 2];    %start nodes
 t=[2 3 4];    %end nodes
 weights=[10 5 7];
 G=graph(s,t, weights);   %create a graph object
 plot(G, 'EdgeLabel', G.Edges.Weight);        %plot the graph
-title('Weighted Undirected Graph'); %add a title
+title('Weighted Undirected Graph'); %add a title9
